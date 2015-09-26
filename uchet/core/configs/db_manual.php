b1acf2cacaaaeac208eb304de80b7dd19927d61a
shikon <yonkon.ru@gmail.com>
2015-09-26 04:56:32 +0300

    uchet config

---
+++ uchet/core/configs/db_manual.php
@@ -0,0 +1,61 @@
+<?
+$dbManualVars = Array('fields', 'variablesName');
+
+$fields = Array(
+	'thema' => 'o_thema',
+	'course' => 'o_course',
+	'worktype' => 'o_type',
+	'client_srok' => 'o_client_srok',
+	'file_part' => 'f_part',
+	'file_arr' => 'ofile',
+	'file_comment' => 'f_comment',
+	'schooltype' => 'o_shcooltype',
+	'country' => 'o_cc1',
+	'city' => 'o_cc2',
+	'vuz' => 'o_vuz',
+	'volume' => 'o_volume',
+	'gost' => 'gost',
+	'font' => 'o_font',
+	'interval' => 'o_interval',
+	'listsource' => 'o_listsource',
+	'addinfo' => 'o_addinfo',
+	'precost' => 'o_precost',
+	'fio' => 'fio',
+	'email' => 'email',
+	'mphone1' => 'mphone1',
+	'mphone2' => 'mphone2',
+	'mphone3' => 'mphone3',
+	'sphone1' => 'sphone1',
+	'sphone2' => 'sphone2',
+	'sphone3' => 'sphone3',
+	'icq' => 'icq',
+	'contact' => 'contact',
+	'fromknow' => 'o_fromknow',
+	'captcha_code' => 'captcha_code',
+	'user' => 'user',
+	'idu' => 'idu',
+	'strong' => 'strong',
+	'act' => 'act',
+	'other_contact' => 'other_contact',
+	'login' => 'login',
+	'pass' => 'pass',
+	'web' => 'web',
+	'skype' => 'skype',
+);
+
+$variablesName = Array(
+	// ParseOrderData
+	'parseorderdata' => 'ParseOrderData',
+	// AddFile
+	'addfile' => 'AddFile',
+	// HiddenInfoClient
+	'hiddeninfoclient' => 'HiddenInfoClient',
+	// ShowInfoClient
+	'showinfoclient' => 'ShowInfoClient',
+	// HiddenInfoOrder
+	'hiddeninfoorder' => 'HiddenInfoOrder',	
+	// ShowInfoOrder
+	'showinfoorder' => 'ShowInfoOrder',
+	//orderform
+	'orderform' => 'orderform'
+);
